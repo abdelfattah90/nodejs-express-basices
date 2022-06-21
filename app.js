37564bdaa674a940b8243dbc61d5f93c3dfda676
unknown <ahmedomar1007@gmail.com>
2022-06-21 17:25:23 +0200

    use chalk & color modules

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const express = require('express')
+import express from 'express'
+import colors from 'colors'
+import chalk from 'chalk'
+
 const app = express()
 const port = 5000
 app.get('/', (req, res) => res.send('In the name of of Allah the Merciful'))
-app.listen(port, () => console.log(`App Running... `))
+app.listen(port, () =>
+  console.log('App Running'.brightGreen.bold + chalk.magenta('...'))
+)
