2d9f2e7172747c9ce8168bc7d0878d4951b1d14a
unknown <ahmedomar1007@gmail.com>
2022-06-22 14:21:36 +0200

    ej - static files - style

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 const port = 5000
 
+app.use(express.static('public'))
+app.use(['/css', '/js', '/images'], express.static('public'))
+
 app.set('view engine', 'ejs')
 app.use(express.json())
 
