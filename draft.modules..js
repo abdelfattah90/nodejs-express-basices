1cf50e0afb9740904269cbba8caa4fa2c50be3b9
unknown <ahmedomar1007@gmail.com>
2022-06-23 16:56:13 +0200

    bcryptjs

--- draft.modules..js
+++ draft.modules..js
@@ -3,12 +3,19 @@ import chalk from 'chalk'
 import validator from 'validator'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import bcrypt from 'bcryptjs'
 
 moment().format()
 dotenv.config()
 app.use(morgan('dev'))
 
-const URL = process.env.URL
+const URL = process.env.PORT
+
+// Bcryptjs - Usage - Sync
+const userPassword = '12345678'
+const hashPassword = bcrypt.hashSync(userPassword, 10)
+console.log(hashPassword)
+console.log(bcrypt.compareSync(userPassword, hashPassword))
 
 console.log(chalk.magenta(moment().format('LLLL')))
 console.log(validator.isMobilePhone('01066078954', 'ar-EG'))
