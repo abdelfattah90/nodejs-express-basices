ee7a97c3e54818c369dab0ba72c4d5b7f60705ab
unknown <ahmedomar1007@gmail.com>
2022-06-25 23:31:25 +0200

    clean code

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Imported Files
 import router from './app/routes/routes.js'
 import postsRouter from './app/routes/posts.js'
 import postIdRouter from './app/routes/post-id.js'
+
 const app = express()
 const port = 5000
 
+// VIEWS
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(['/css', '/js', '/images'], express.static('public'))
+app.set('views', path.join(dirname, './app/views'))
+app.use(express.static('./app/public'))
 
+// ROUETS
 app.use(express.json())
 app.use([router, postsRouter, postIdRouter])
 
