18084ec93d83fef91fe2fd53d29bc971b4934fa2
unknown <ahmedomar1007@gmail.com>
2022-07-17 07:21:15 +0200

    readline

--- .node-draft/readline.js
+++ .node-draft/readline.js
@@ -9,3 +9,7 @@ rLine.question(`What's your name? \n`, (name) => {
   console.log(`Hi ${name}!`)
   rLine.close()
 })
+
+// inquirer
+// https://www.npmjs.com/package/inquirer
+// A collection of common interactive command line user interfaces.
